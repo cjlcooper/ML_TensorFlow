e934233660579ba48ef2dd88cd26381493d21e40
cjlcooper <cjlcooper@163.com>
2017-12-29 22:14:07 +0800

    finish

--- CurveFitting.py
+++ CurveFitting.py
@@ -15,3 +15,15 @@ y = k * x_data + b
 loss = tf.reduce_mean(tf.square(y - y_data))#get ping jun zhi
 optimizer = tf.train.GradientDescentOptimizer(0.5)
 train = optimizer.minimize(loss)
+
+# init tensor Variable
+init = tf.global_variables_initializer()
+
+# Run session
+sess = tf.Session()
+sess.run(init)
+
+for step in xrange(201):
+	sess.run(train)
+	if step % 20 == 0:
+		print(sess.run(k), sess.run(b))
